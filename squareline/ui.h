94d8dc58c5cc053c3e6d6006e46256af1a499f82
steba77 <s.bandi@sevio.it>
2024-01-19 11:53:21 +0100

    t1

--- squareline/ui.h
+++ squareline/ui.h
@@ -10,65 +10,65 @@
 extern "C" {
 #endif
 
-    #include "lvgl/lvgl.h"
+#include "lvgl/lvgl.h"
 
-extern lv_obj_t *ui_Home;
-extern lv_obj_t *ui_Screen1_Image1;
-extern lv_obj_t *ui_Screen1_Label1;
-extern lv_obj_t *ui_boardStatusLabel;
-extern lv_obj_t *ui_uptimeLabel;
-extern lv_obj_t *ui_dateTimeLabel;
-extern lv_obj_t *ui_VPNSTATUS;
-extern lv_obj_t *ui_Screen2_Label4;
-extern lv_obj_t *ui_CONNECTED_PORT;
-extern lv_obj_t *ui_DHCP_NEG_Panel;
-extern lv_obj_t *ui_GATEWAY_panel;
-extern lv_obj_t *ui_INTERNET_CONN_Panel;
-extern lv_obj_t *ui_VPNSTATUS_Label1;
-extern lv_obj_t *ui_VPNSTATUS_Label2;
-extern lv_obj_t *ui_VPNSTATUS_Label3;
-extern lv_obj_t *ui_VPNSTATUS_Label4;
-extern lv_obj_t *ui_PORTS_Panel;
-extern lv_obj_t *ui_VPNSTATUS_Label5;
-extern lv_obj_t *ui_VPN_Panel;
-extern lv_obj_t *ui_VPNSTATUS_Label6;
-extern lv_obj_t *ui_VPNSTATUS_Image1;
-extern lv_obj_t *ui_WANIPCONFIG;
-extern lv_obj_t *ui_Screen2_Label1;
-extern lv_obj_t *ui_WANIPCONFIG_Panel2;
-extern lv_obj_t *ui_WANIPCONFIG_Label1;
-extern lv_obj_t *ui_WANIPCONFIG_Label2;
-extern lv_obj_t *ui_WANIPCONFIG_Label3;
-extern lv_obj_t *ui_WANIPCONFIG_Image1;
-extern lv_obj_t *ui_WANIPCONFIG_Label4;
-extern lv_obj_t *ui_WANIPCONFIG_Image2;
-extern lv_obj_t *ui_WANIPCONFIG_Button_RG;
-extern lv_obj_t *ui_WANIPCONFIG_ButtonDN;
-extern lv_obj_t *ui_WANIPCONFIG_Button_LF;
-extern lv_obj_t *ui_LANIPCONFIG;
-extern lv_obj_t *ui_Screen2_Label3;
-extern lv_obj_t *ui_WANIPCONFIG_Panel3;
-extern lv_obj_t *ui_WANIPCONFIG_Label9;
-extern lv_obj_t *ui_WANIPCONFIG_Label10;
-extern lv_obj_t *ui_WANIPCONFIG_Image5;
-extern lv_obj_t *ui_WANIPCONFIG_Label12;
-extern lv_obj_t *ui_WANIPCONFIG_Image6;
-extern lv_obj_t *ui_WLANIPCONFIG;
-extern lv_obj_t *ui_Screen2_Label2;
-extern lv_obj_t *ui_WANIPCONFIG_Panel1;
-extern lv_obj_t *ui_WANIPCONFIG_Label5;
-extern lv_obj_t *ui_WANIPCONFIG_Label6;
-extern lv_obj_t *ui_WANIPCONFIG_Label7;
-extern lv_obj_t *ui_WANIPCONFIG_Image3;
-extern lv_obj_t *ui_WANIPCONFIG_Label8;
-extern lv_obj_t *ui_WANIPCONFIG_Image4;
+extern lv_obj_t * ui_Home;
+extern lv_obj_t * ui_Screen1_Image1;
+extern lv_obj_t * ui_Screen1_Label1;
+extern lv_obj_t * ui_boardStatusLabel;
+extern lv_obj_t * ui_uptimeLabel;
+extern lv_obj_t * ui_dateTimeLabel;
+extern lv_obj_t * ui_VPNSTATUS;
+extern lv_obj_t * ui_Screen2_Label4;
+extern lv_obj_t * ui_CONNECTED_PORT;
+extern lv_obj_t * ui_DHCP_NEG_Panel;
+extern lv_obj_t * ui_GATEWAY_panel;
+extern lv_obj_t * ui_INTERNET_CONN_Panel;
+extern lv_obj_t * ui_VPNSTATUS_Label1;
+extern lv_obj_t * ui_VPNSTATUS_Label2;
+extern lv_obj_t * ui_VPNSTATUS_Label3;
+extern lv_obj_t * ui_VPNSTATUS_Label4;
+extern lv_obj_t * ui_PORTS_Panel;
+extern lv_obj_t * ui_VPNSTATUS_Label5;
+extern lv_obj_t * ui_VPN_Panel;
+extern lv_obj_t * ui_VPNSTATUS_Label6;
+extern lv_obj_t * ui_VPNSTATUS_Image1;
+extern lv_obj_t * ui_WANIPCONFIG;
+extern lv_obj_t * ui_Screen2_Label1;
+extern lv_obj_t * ui_WANIPCONFIG_Panel2;
+extern lv_obj_t * ui_WANIPCONFIG_Label1;
+extern lv_obj_t * ui_WANIPCONFIG_Label2;
+extern lv_obj_t * ui_WANIPCONFIG_Label3;
+extern lv_obj_t * ui_WANIPCONFIG_Image1;
+extern lv_obj_t * ui_WANIPCONFIG_Label4;
+extern lv_obj_t * ui_WANIPCONFIG_Image2;
+extern lv_obj_t * ui_WANIPCONFIG_Button_RG;
+extern lv_obj_t * ui_WANIPCONFIG_ButtonDN;
+extern lv_obj_t * ui_WANIPCONFIG_Button_LF;
+extern lv_obj_t * ui_LANIPCONFIG;
+extern lv_obj_t * ui_Screen2_Label3;
+extern lv_obj_t * ui_WANIPCONFIG_Panel3;
+extern lv_obj_t * ui_WANIPCONFIG_Label9;
+extern lv_obj_t * ui_WANIPCONFIG_Label10;
+extern lv_obj_t * ui_WANIPCONFIG_Image5;
+extern lv_obj_t * ui_WANIPCONFIG_Label12;
+extern lv_obj_t * ui_WANIPCONFIG_Image6;
+extern lv_obj_t * ui_WLANIPCONFIG;
+extern lv_obj_t * ui_Screen2_Label2;
+extern lv_obj_t * ui_WANIPCONFIG_Panel1;
+extern lv_obj_t * ui_WANIPCONFIG_Label5;
+extern lv_obj_t * ui_WANIPCONFIG_Label6;
+extern lv_obj_t * ui_WANIPCONFIG_Label7;
+extern lv_obj_t * ui_WANIPCONFIG_Image3;
+extern lv_obj_t * ui_WANIPCONFIG_Label8;
+extern lv_obj_t * ui_WANIPCONFIG_Image4;
 
 
-LV_IMG_DECLARE( ui_img_logo_png);   // assets/logo.png
-LV_IMG_DECLARE( ui_img_vpn_png);   // assets/vpn.png
-LV_IMG_DECLARE( ui_img_eth_img_png);   // assets/eth_img.png
-LV_IMG_DECLARE( ui_img_frecce_d_r_png);   // assets/frecce_d_r.png
-LV_IMG_DECLARE( ui_img_wifi_img2_png);   // assets/wifi_img2.png
+LV_IMG_DECLARE(ui_img_logo_png);    // assets\logo.png
+LV_IMG_DECLARE(ui_img_vpn_png);    // assets\vpn.png
+LV_IMG_DECLARE(ui_img_eth_img_png);    // assets\eth_img.png
+LV_IMG_DECLARE(ui_img_frecce_d_r_png);    // assets\frecce_d_r.png
+LV_IMG_DECLARE(ui_img_wifi_img2_png);    // assets\wifi_img2.png
 
 
 
